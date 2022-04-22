767b7ad68f0922e95449f5949b09b463afd2921a
mofe <mofeomotosho8@gmail.com>
2022-04-21 23:16:45 -0300

    setting up

---
+++ src/Components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../Styling/App.css';
+import Main from './Main';
+
+function App() {
+  return (
+    <div className="App">
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Styling/Main.css';
+
+function Main(){
+    return(
+        <div className='main'>
+            <p>mofe</p>
+        </div>
+    )
+};
+
+export default Main;
